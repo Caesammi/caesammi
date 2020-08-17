28fea7f2766af1f35c0cb8ba997f98f50ce2b968
chi.zhang <chi.zhang@pcitc.com>
2020-08-17 23:18:05 +0800

    s

--- src/components/mytest.vue
+++ src/components/mytest.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="myMain">
     <Pie :id="idP1" :option="optionP1"></Pie>
-    <histogram :id="idH1" :option="optionH1"></histogram>
+    <div style="width: 1000px">
+      <histogram :id="idH1" :option="optionH1"></histogram>
+    </div>
   </div>
 </template>
 
@@ -21,57 +23,87 @@ export default {
       idH1: 'testH',
       optionH1: {
         chart: {
+          renderTo: 'container',
           type: 'column',
+          marginRight: 30,
+          height: 500,
           options3d: {
             enabled: true,
-            alpha: 15,
-            beta: 15,
-            viewDistance: 25,
-            depth: 40
+            alpha: 4,
+            beta: 8,
+            depth: 50,
+            viewDistance: 25
           },
-          marginTop: 80,
-          marginRight: 40
-        },
+          spacing:[10,10,10,10]
+        },colors: ['#058DC7', '#50B432', '#ED561B', '#DDDF00',
+          '#24CBE5', '#64E572', '#FF9655', '#FFF263', '#6AF9C4'],
         title: {
-          text: '以性别划分的水果消费总量'
+          text: '交互性3D柱状图'
         },
-        xAxis: {
-          categories: ['苹果', '橘子', '梨', '葡萄', '香蕉']
-        },
-        yAxis: {
-          allowDecimals: false,
-          min: 0,
-          title: {
-            text: '水果数量'
-          }
-        },
-        tooltip: {
-          headerFormat: '<b>{point.key}</b><br>',
-          pointFormat: '<span style="color:{series.color}">\u25CF</span> {series.name}: {point.y} / {point.stackTotal}'
+        subtitle: {
+          text: '可通过滑动下方滑块测试'
         },
         plotOptions: {
           column: {
+            depth: 30,
+            opacity:0.8,
             stacking: 'normal',
-            depth: 40
-          }
+            grouping: false,groupZPadding: 1
+          },
+        },	xAxis: [{
+          categories: [
+            '1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5'
+          ],
+          tickWidth: 2,
+          tickLength: 10,
+          labels: {
+            align: 'left',
+            x: 0,
+            y: 25,
+          },
+        },{
+          categories: [
+            '2015年', '2', '3', '4', '5','2015年', '2', '3', '4', '5','2015年', '2', '3', '4', '5','2015年', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5'
+          ],
+          gridLineWidth: 0,
+          minTickInterval: 5,
+          labels: {whiteSpace: 'nowrap',
+            textOverflow: 'none',
+            step: 1,
+            x: 0,
+            y: 0,
+          },
         },
+        ],
         series: [{
-          name: '小张',
-          data: [5, 3, 4, 7, 2],
-          stack: 'male'
-        }, {
-          name: '小王',
-          data: [3, 4, 4, 2, 5],
-          stack: 'male'
-        }, {
-          name: '小彭',
-          data: [2, 5, 6, 2, 1],
-          stack: 'female'
-        }, {
-          name: '小潘',
-          data: [3, 0, 4, 4, 3],
-          stack: 'female'
-        }]
+          name: '温度',
+          type: 'spline',
+          data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6,7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6,7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+          tooltip: {
+            valueSuffix: ' °C'
+          },
+          stack: 0
+        },
+          {
+            name: '温度',
+            type: 'spline',
+            data: [8.0, 116.9, 449.5, 514.5, 218.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6,8.0, 116.9, 449.5, 514.5, 218.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6,8.0, 116.9, 449.5, 514.5, 218.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
+            tooltip: {
+              valueSuffix: ' °C'
+            },
+            stack: 0
+          },{
+            name:'图例1',
+            type: 'column',
+            xAxis: 1,//第二个X轴
+            data: [1.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4,1.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4,1.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+            stack: 1
+          },{
+            name:'图例1',
+            type: 'column',
+            data: [1.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
+            stack: 1
+          },]
       },
       idP1:'testP',
       optionP1:{
